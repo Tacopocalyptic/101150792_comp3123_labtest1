3e2c73552d172b8c825c887c135d7f159b419c74
Tacopocalyptic <130405272+Tacopocalyptic@users.noreply.github.com>
2024-10-03 19:40:23 -0400

    question 3 done

--- question3/add.js
+++ question3/add.js
@@ -1,4 +1,22 @@
 // Josephine Snyder - 101150792
 // Question 3
 
-const readFile = require('fs')
+const fs = require('fs');
+const path = require('path');
+
+// number of files to create
+let k = 10
+
+// checking for and creating 'logs' directory
+let dir = path.join(__dirname, "logs")
+if(fs.existsSync(dir)){
+    console.log(`Directory ${dir} already exists.`)
+}else{
+    fs.mkdirSync(dir)
+}
+
+// writing log files. Overwriting if exists.
+for (let i = 0; i < k; i++) {
+    fs.writeFileSync(path.join(dir, `Log${i}.txt`), `HELLO from Log ${i}`)
+    console.log(`Log${i}.txt`)
+}
--- question3/remove.js
+++ question3/remove.js
@@ -1,4 +1,27 @@
 // Josephine Snyder - 101150792
 // Question 3
 
-const readFile = require('fs')
+const fs = require('fs');
+const path = require('path');
+
+let dir = path.join(__dirname, "logs")
+
+// checking for 'logs' directory
+fs.readdir(dir, (err, files) => {
+    if (err) console.log(err +" No action taken");
+  
+    // deleting files
+    else {
+        for (let file of files) {
+            console.log("delete files..."+file)
+            fs.unlink(path.join(dir, file), (err) => {
+                if (err) console.log(err)
+            })
+        }
+    }
+})
+
+fs.rmdir(dir, () => {
+    console.log("'logs' directory Gone");
+});
+
